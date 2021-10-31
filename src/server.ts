d46ed4c3c18e87917fd2ccca88909a3494a27671
Julio Mojica <juliocmojica@gmail.com>
2021-10-31 12:11:24 -0400

    controllers init setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import productRouter from "./api/controllers/products.controller";
+import userRouter from "./api/controllers/user.controller";
 
 dotenv.config();
 const app: express.Application = express();
@@ -9,6 +11,9 @@ app.use(express.json());
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
+
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
 app.listen(3000, function () {
   console.log(`starting app on port: 3000`);
