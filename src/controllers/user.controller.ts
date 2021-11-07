b6bd4ac3c5623600559d67df0294f199fd33311a
Julio Mojica <juliocmojica@gmail.com>
2021-11-07 11:05:47 -0500

    create user and return token

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,30 @@
 import express, { Request, Response } from "express";
-import checkAuthorization from "../middleware/checkAuthorization";
+import bcrypt from 'bcrypt';
+import emailValidator from 'email-validator';
+import jwt from 'jsonwebtoken';
+import { insertUser } from '../models/user.model'
 
 const userRouter = express.Router();
 
+const createJWT = (id: string): string => jwt.sign(id, process.env.TOKEN_SECRET!)
+
 userRouter.post('/signup', async (req: Request, res: Response) => {
   try {
+    if(!req.body.email || !req.body.password || !req.body.first_name || !req.body.last_name) {
+      return res.status(400).json({ error: 'missing required parameter'})
+    }
 
+    if(!emailValidator.validate(req.body.email)) {
+      return res.status(400).json({ error: 'Please provide valid email'})
+    }
+
+    const hashedPass = bcrypt.hashSync(`${req.body.password}${process.env.PEPPER}`, 10)
+
+    const user = await insertUser(req.body.email, req.body.first_name, req.body.last_name, hashedPass)
+
+    const token = createJWT(user.id)
+
+    return res.status(200).json({ message: 'user created successfully', token })
   } catch (err) {
     res.status(500).json({ error: 'Internal application error' })
   }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,7 +2,7 @@ import Client  from '../db';
 
 export const insertUser = async (email: string, first_name: string, last_name: string, password: string) => {
     const client = await Client.connect();
-    const insertQuery = 'INSERT INTO users (email, firstname, lastname, password) VALUES($1, $2, $3, $4) RETURNING email';
+    const insertQuery = 'INSERT INTO users (email, firstname, lastname, password) VALUES($1, $2, $3, $4) RETURNING *';
     const results = await client.query(insertQuery, [email, first_name, last_name, password])
     client.release()
     return results.rows[0];
