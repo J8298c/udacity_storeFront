2ba9e502b37f4d0bd5a6217ec3669866fed96511
Julio Mojica <juliocmojica@gmail.com>
2021-11-10 20:44:24 -0500

    add product router to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import userRouter from "./controllers/user.controller";
+import productRouter from "./controllers/product.controller";
 
 dotenv.config();
 const app: express.Application = express();
@@ -8,6 +9,7 @@ const app: express.Application = express();
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.listen(3000, function () {
   console.log(`starting app on port: 3000`);
