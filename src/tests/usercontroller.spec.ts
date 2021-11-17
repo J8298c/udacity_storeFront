e571184cfb7a9673f0126371da9b398fcc192d0a
Julio Mojica <juliocmojica@gmail.com>
2021-11-17 06:39:33 -0500

    bad request test

--- src/tests/usercontroller.spec.ts
+++ src/tests/usercontroller.spec.ts
@@ -4,11 +4,21 @@ import app from '../server';
 describe('User Controller Intergration Test', () => {
   const request = supertest(app);
   describe('[POST]/signup', () => {
-    it('should sign a user in successfully', async () => {
-      const body = { email: 'mytestuser@mail.com', password: 'abcd1234', first_name: 'test', last_name: 'user'}
-      const response = await request.post('/api/users/signup').send(body)
-      expect(response.status).toEqual(200)
-      expect(response.body.token).toBeDefined()
+    describe('successful signup', () => {
+      it('should sign a user in successfully', async () => {
+        const body = { email: 'mytestuser@mail.com', password: 'abcd1234', first_name: 'test', last_name: 'user'}
+        const response = await request.post('/api/users/signup').send(body)
+        expect(response.status).toEqual(200)
+        expect(response.body.token).toBeDefined()
+      })
+    })
+
+    describe('unsuccessful user signup', () => {
+      it('should fail if missing required parameter and return 400', async () => {
+        const body = { email: 'baduser@mail.com', password: '1234', last_name: 'bad request'}
+        const response = await request.post('/api/users/signup').send(body)
+        expect(response.status).toEqual(400)
+      })
     })
   })
 })
