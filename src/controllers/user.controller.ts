f07781e4e87c113493061a29c7494d8a50372b27
Julio Mojica <juliocmojica@gmail.com>
2021-11-18 09:27:52 -0500

    Format user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,70 +1,93 @@
 import express, { Request, Response } from "express";
-import bcrypt from 'bcrypt';
-import emailValidator from 'email-validator';
-import jwt from 'jsonwebtoken';
-import { insertUser, getUserByEmail, fetchUsers } from '../models/user.model'
+import bcrypt from "bcrypt";
+import emailValidator from "email-validator";
+import jwt from "jsonwebtoken";
+import { insertUser, getUserByEmail, fetchUsers } from "../models/user.model";
 import checkAuthorization from "../middleware/checkAuthorization";
 
 const userRouter = express.Router();
 
-const internalAppError = 'Internal application error'
+const internalAppError = "Internal application error";
 
-const createJWT = (id: string): string => jwt.sign({userId: id}, process.env.TOKEN_SECRET!)
+const createJWT = (id: string): string =>
+  jwt.sign({ userId: id }, process.env.TOKEN_SECRET!);
 
-userRouter.post('/signup', async (req: Request, res: Response) => {
+userRouter.post("/signup", async (req: Request, res: Response) => {
   try {
-    if(!req.body.email || !req.body.password || !req.body.first_name || !req.body.last_name) {
-      return res.status(400).json({ error: 'missing required parameter'})
+    if (
+      !req.body.email ||
+      !req.body.password ||
+      !req.body.first_name ||
+      !req.body.last_name
+    ) {
+      return res.status(400).json({ error: "missing required parameter" });
     }
 
-    if(!emailValidator.validate(req.body.email)) {
-      return res.status(400).json({ error: 'Please provide valid email'})
+    if (!emailValidator.validate(req.body.email)) {
+      return res.status(400).json({ error: "Please provide valid email" });
     }
 
-    const hashedPass = bcrypt.hashSync(`${req.body.password}${process.env.PEPPER}`, 10)
+    const hashedPass = bcrypt.hashSync(
+      `${req.body.password}${process.env.PEPPER}`,
+      10
+    );
 
-    const user = await insertUser(req.body.email, req.body.first_name, req.body.last_name, hashedPass)
+    const user = await insertUser(
+      req.body.email,
+      req.body.first_name,
+      req.body.last_name,
+      hashedPass
+    );
 
-    const token = createJWT(user.id)
+    const token = createJWT(user.id);
 
-    return res.status(200).json({ message: 'user created successfully', token })
+    return res
+      .status(200)
+      .json({ message: "user created successfully", token });
   } catch (err) {
-    return res.status(500).json({ error: internalAppError })
+    return res.status(500).json({ error: internalAppError });
   }
 });
 
-userRouter.post('/login', async (req: Request, res: Response) => {
+userRouter.post("/login", async (req: Request, res: Response) => {
   try {
     if (!req.body.email || !req.body.password) {
-      return res.status(400).json({ error: 'missing required params'})
+      return res.status(400).json({ error: "missing required params" });
     }
-    const user = await getUserByEmail(req.body.email)
-    const badEmailPassComboMsg = 'Bad email password combo'
-    if(!user.password) {
-      return res.status(400).json({ error: badEmailPassComboMsg})
-    }
-
-    const passwordCheck = bcrypt.compareSync(`${req.body.password}${process.env.PEPPER}`, user.password)
-
-    if(!passwordCheck) {
-      return res.status(400).json({ error: badEmailPassComboMsg })
+    const user = await getUserByEmail(req.body.email);
+    const badEmailPassComboMsg = "Bad email password combo";
+    if (!user.password) {
+      return res.status(400).json({ error: badEmailPassComboMsg });
     }
 
-    const token = createJWT(user.id!.toString())
+    const passwordCheck = bcrypt.compareSync(
+      `${req.body.password}${process.env.PEPPER}`,
+      user.password
+    );
 
-    return res.status(200).json({ message: 'Login success', token })
+    if (!passwordCheck) {
+      return res.status(400).json({ error: badEmailPassComboMsg });
+    }
+
+    const token = createJWT(user.id!.toString());
+
+    return res.status(200).json({ message: "Login success", token });
   } catch (err) {
-    return res.status(500).json({ error: internalAppError})
+    return res.status(500).json({ error: internalAppError });
   }
-})
+});
 
-userRouter.get('/all', checkAuthorization, async (req: Request, res: Response) => {
-  try {
-    const allUsersEmails = await fetchUsers()
-    return res.status(200).json({ allUsersEmails })
-  } catch (err) {
-    return res.status(500).json({ error: internalAppError})
+userRouter.get(
+  "/all",
+  checkAuthorization,
+  async (req: Request, res: Response) => {
+    try {
+      const allUsersEmails = await fetchUsers();
+      return res.status(200).json({ allUsersEmails });
+    } catch (err) {
+      return res.status(500).json({ error: internalAppError });
+    }
   }
-})
+);
 
 export default userRouter;
