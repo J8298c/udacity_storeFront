24fa136d0e47bcebd67445e6b25c87492773aa8b
Julio Mojica <juliocmojica@gmail.com>
2021-11-18 09:28:19 -0500

    Format user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,27 +1,37 @@
-import Client  from '../db';
-import { User } from '../interfaces/user';
+import Client from "../db";
+import { User } from "../interfaces/user";
 
-export const insertUser = async (email: string, first_name: string, last_name: string, password: string) => {
-    const client = await Client.connect();
-    const insertQuery = 'INSERT INTO users (email, firstname, lastname, password) VALUES($1, $2, $3, $4) RETURNING *';
-    const results = await client.query(insertQuery, [email, first_name, last_name, password])
-    client.release()
-    return results.rows[0];
-}
+export const insertUser = async (
+  email: string,
+  first_name: string,
+  last_name: string,
+  password: string
+) => {
+  const client = await Client.connect();
+  const insertQuery =
+    "INSERT INTO users (email, firstname, lastname, password) VALUES($1, $2, $3, $4) RETURNING *";
+  const results = await client.query(insertQuery, [
+    email,
+    first_name,
+    last_name,
+    password,
+  ]);
+  client.release();
+  return results.rows[0];
+};
 
 export const getUserByEmail = async (email: string): Promise<User> => {
-    const client = await Client.connect();
-    const fetchQuery = 'SELECT password, id FROM users WHERE email=($1)'
-    const results = await client.query(fetchQuery, [email])
-    client.release()
-    return results.rows[0]
-}
+  const client = await Client.connect();
+  const fetchQuery = "SELECT password, id FROM users WHERE email=($1)";
+  const results = await client.query(fetchQuery, [email]);
+  client.release();
+  return results.rows[0];
+};
 
 export const fetchUsers = async (): Promise<User[]> => {
-    const client = await Client.connect();
-    const fetchQuery = 'SELECT email FROM users'
-    const results = await client.query(fetchQuery)
-    client.release()
-    return results.rows;
-}
-
+  const client = await Client.connect();
+  const fetchQuery = "SELECT email FROM users";
+  const results = await client.query(fetchQuery);
+  client.release();
+  return results.rows;
+};
