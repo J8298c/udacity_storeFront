54a9bb328741e3eca77d435d9e67b7ecff28eea6
Julio Mojica <juliocmojica@gmail.com>
2021-11-18 09:27:07 -0500

    Insert Product

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,33 +1,48 @@
-import Client from '../db';
-import { Product } from '../interfaces/products';
+import Client from "../db";
+import { Product } from "../interfaces/products";
 
 export const fetchAllProducts = async (): Promise<Product[]> => {
-  const client = await Client.connect()
-  const fetchQuery = 'SELECT * FROM products';
-  const results = await client.query(fetchQuery);
-  client.release()
-  return results.rows;
-}
-
-export const fetchSingleProduct = async (id: number) :Promise<Product> => {
-  const client = await Client.connect()
-  const fetchQuery = 'SELECT * FROM products WHERE id=($1)'
-  const results = await client.query(fetchQuery, [id])
-  client.release()
-  return results.rows[0];
-}
-
-export const orderProduct = async (productId: number, userId: number, quantity: number) => {
   const client = await Client.connect();
-  const insertQuery = 'INSERT INTO products_orders (quantity, product_id, user_id) VALUES($1, $2, $3)'
-  await client.query(insertQuery, [quantity, productId, userId])
-  client.release()
-}
+  const fetchQuery = "SELECT * FROM products";
+  const results = await client.query(fetchQuery);
+  client.release();
+  return results.rows;
+};
+
+export const createNewProduct = async (
+  name: string,
+  price: number
+): Promise<void> => {
+  const client = await Client.connect();
+  const insertQuery = "INSERT INTO products (name, price) VALUES($1, $2)";
+  await client.query(insertQuery, [name, price]);
+  client.release();
+};
+
+export const fetchSingleProduct = async (id: number): Promise<Product> => {
+  const client = await Client.connect();
+  const fetchQuery = "SELECT * FROM products WHERE id=($1)";
+  const results = await client.query(fetchQuery, [id]);
+  client.release();
+  return results.rows[0];
+};
+
+export const orderProduct = async (
+  productId: number,
+  userId: number,
+  quantity: number
+) => {
+  const client = await Client.connect();
+  const insertQuery =
+    "INSERT INTO products_orders (quantity, product_id, user_id) VALUES($1, $2, $3)";
+  await client.query(insertQuery, [quantity, productId, userId]);
+  client.release();
+};
 
 export const fetchOrders = async () => {
   const client = await Client.connect();
-  const fetchQuery = 'SELECT * FROM products_orders';
-  const results = await client.query(fetchQuery)
-  client.release()
-  return results.rows
-}
+  const fetchQuery = "SELECT * FROM products_orders";
+  const results = await client.query(fetchQuery);
+  client.release();
+  return results.rows;
+};
