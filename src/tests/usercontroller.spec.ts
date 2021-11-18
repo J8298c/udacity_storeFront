d99c7ca0b99690c0c47e4cc1eef8e87076d6340d
Julio Mojica <juliocmojica@gmail.com>
2021-11-17 20:23:31 -0500

    user controller

--- src/tests/usercontroller.spec.ts
+++ src/tests/usercontroller.spec.ts
@@ -26,4 +26,22 @@ describe('User Controller Intergration Test', () => {
       })
     })
   })
+
+  describe('[POST]/login', () => {
+    describe('should log in user successfully', () => {
+      const goodUser = { email: 'mytestuser@mail.com', password: 'abcd1234' };
+      it('should log user in and return a token', async () => {
+        const response = await request.post('/api/users/login').send(goodUser)
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeDefined();
+      })
+    })
+    describe('bad password login', () => {
+      it('should fail if password is wrong', async () => {
+        const badUser = { email: 'mytestuser@mail.com', password: 'abcd1' };
+        const response = await request.post('/api/users/login').send(badUser)
+        expect(response.status).toEqual(400)
+      })
+    })
+  })
 })
